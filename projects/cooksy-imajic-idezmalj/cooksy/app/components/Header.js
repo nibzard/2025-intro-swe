1fd19ac814586a4d6e2aa75b2c518e96c32723df
maiva667 <ivamajic03@gmail.com>
2025-12-12 21:33:43 +0100

    add header and navigation

---
+++ projects/cooksy-imajic-idezmalj/cooksy/app/components/Header.js
@@ -0,0 +1,22 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Header() {
+    const pathname = usePathname();
+  return (
+    <div className="flex justify-between items-center relative bg-[#D7C0A9] p-7 shadow-md">
+      <p className="text-black text-4xl text-bold">
+        <span className="text-[#5B3705]">C</span>
+        <span className="text-[#6E5430]">o</span>
+        <span className="text-[#6E5430]">o</span>
+        <span className="text-[#5B3705]">ksy</span>
+      </p>
+      <nav className="hidden md:flex space-x-8">
+        <Link href={"/"} className={pathname === "/" ? "text-[#5B3705]" : "text-black"}>HOME</Link>
+        <Link href={"/recipes"} className={pathname === "/recipes" ? "text-[#5B3705]" : "text-black"}>RECIPES</Link>
+        <Link href={"/favorites"} className={pathname === "/favorites" ? "text-[#5B3705]" : "text-black"}>FAVORITES</Link>
+      </nav>
+    </div>
+  );
+}
--- projects/cooksy-imajic-idezmalj/cooksy/app/layout.js
+++ projects/cooksy-imajic-idezmalj/cooksy/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +21,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#F6EEE2]`}
       >
-        {children}
+        <Header />
+        <main className="flex-grow">
+          {children}
+        </main>
       </body>
     </html>
   );
