04342a810ea5441727877f814c8d4fc9141799b1
Lucija Tomić <lucijatomic456@gmail.com>
2026-01-18 17:45:54 +0000

    Update: Ažuriran projekt s popravljenom karticom

---
+++ projects/paradizot-kcurcija-ekevic-ltomic-jvitaljic/promet_split/src/pages/Map.jsx
@@ -0,0 +1,11 @@
+import BusMap from '../components/BusMap';
+
+const MapPage = () => {
+    return (
+        <div style={{ height: '100%', width: '100%' }}>
+            <BusMap />
+        </div>
+    );
+};
+
+export default MapPage;
