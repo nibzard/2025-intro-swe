76feb30f853586f31de884f34f7b7bd1313cd305
danrog101 <dana.rogulj01@email.com>
2026-01-21 15:02:25 +0100

    obrisain backend i frontend

--- projects/povezivanje-timova-Rogulj-Mihaljevic/backend/routes/waitlistRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { joinWaitlist, leaveWaitlist } = require('../controllers/waitlistController');
-const auth = require('../middleware/auth');
-
-router.post('/:teamId/join', auth, joinWaitlist);
-router.post('/:teamId/leave', auth, leaveWaitlist);
-
-module.exports = router;
